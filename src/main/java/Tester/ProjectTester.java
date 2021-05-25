e1259b794fef0c23bf112419590646f8cd6ffadc
Darrensow <Darrensow@hotmail.com>
2021-05-26 03:10:54 +0800

    Added comments in ProjectTester.java

--- src/main/java/Tester/ProjectTester.java
+++ src/main/java/Tester/ProjectTester.java
@@ -9,10 +9,17 @@ import Semag.Issue;
  */
 public class ProjectTester {
     public static void main(String[] args) {
+        /*
+            Please take note that these printing are just for testing, the main json file is data.json.
+            For now, troubleshooting for Comment.java and Issue.java are using both Comment.json and Issue.json
+            Once the troubleshooting for Project and Issue is done, the data.json file will be used as the main storage location
+         */
+        //Try to print from the Comment.json
         Comment cmt = new Comment();
         cmt.loadData();
         System.out.println(cmt.toString());
 
+        //Try to prin from the Issue.json
         Issue isu = new Issue();
         isu.loadData();
         isu.print();
